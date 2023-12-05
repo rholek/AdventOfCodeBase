a6727e6927667cb5dbd299facaa49df0ca0227ca
Radim Holek <rholek@idc.com>
2023-12-05 18:25:29 +0100

    Improve PathFinding

--- AdventOfCode/StandardAlgorithms/PathFinding.cs
+++ AdventOfCode/StandardAlgorithms/PathFinding.cs
@@ -1,11 +1,11 @@
 ﻿namespace AdventOfCode.StandardAlgorithms;
 
-//TODO add option to get entire path
-//TODO tests
 public class PathFinding<T> where T : notnull
 {
     private readonly Func<T, T, long> costFunction;
     private readonly Func<T, IEnumerable<T>> reachableFunction;
+
+    public PathFinding(Func<T, IEnumerable<T>> reachableFunction) : this((_, _) => 1, reachableFunction) { }
 
     public PathFinding(Func<T, T, long> costFunction, Func<T, IEnumerable<T>> reachableFunction)
     {
@@ -20,39 +20,7 @@ public class PathFinding<T> where T : notnull
 
     public bool TryGetCost(T startPoint, Func<T, bool> endReached, out long totalCost)
     {
-        var toProcess = new Dictionary<T, long>
-        {
-            { startPoint, 0 }
-        };
-
-        var processed = new HashSet<T>();
-        while (toProcess.Any())
-        {
-            var minimalCost = toProcess.Where(x => !processed.Contains(x.Key)).OrderBy(x => x.Value).Select(x => x.Key).First();
-            processed.Add(minimalCost);
-
-            foreach (var p in reachableFunction(minimalCost))
-            {
-                if (processed.Contains(p))
-                    continue;
-
-                var cost = toProcess[minimalCost] + costFunction(minimalCost, p);
-
-                if (toProcess.GetValueOrDefault(p, long.MaxValue) > cost)
-                    toProcess[p] = cost;
-
-                if (!endReached(p))
-                    continue;
-                
-                totalCost = toProcess[p];
-                return true;
-            }
-
-            toProcess.Remove(minimalCost);
-        }
-
-        totalCost = long.MaxValue;
-        return false;
+        return TryGetCost(startPoint, endReached, out totalCost, out _);
     }
 
     public long GetCost(T startPoint, T endPoint)
@@ -62,11 +30,91 @@ public class PathFinding<T> where T : notnull
         throw new Exception("Path not found.");
     }
 
-
     public long GetCost(T startPoint, Func<T, bool> endReached)
     {
         if (TryGetCost(startPoint, endReached, out var totalCost))
             return totalCost;
         throw new Exception("Path not found.");
     }
+
+    public bool TryGetPath(T startPoint, T endPoint, out IEnumerable<T> bestPath)
+    {
+        if (!TryGetCost(startPoint, p => Equals(p, endPoint), out _, out var neighbors))
+        {
+            bestPath = new List<T>();
+            return false;
+        }
+
+        var pathUsed = new List<T>();
+        var current = endPoint;
+        do
+        {
+            pathUsed.Add(current);
+            current = neighbors[current];
+        } while (!Equals(current, startPoint));
+
+        pathUsed.Add(startPoint);
+
+        bestPath = pathUsed.AsEnumerable().Reverse().ToList();
+
+        return true;
+    }
+
+    public IEnumerable<T> GetPath(T startPoint, T endPoint)
+    {
+        if (TryGetPath(startPoint, endPoint, out var path))
+            return path;
+        throw new Exception("Path not found.");
+    }
+
+    private bool TryGetCost(T startPoint, Func<T, bool> endReached, out long totalCost, out Dictionary<T, T> bestPath)
+    {
+        var minCosts = new Dictionary<T, long>
+        {
+            { startPoint, 0 }
+        };
+
+        bestPath = new Dictionary<T, T>();
+
+        var processed = new HashSet<T>();
+        while (minCosts.Any())
+        {
+            var minimalCost = minCosts
+                .Where(x => !processed.Contains(x.Key))
+                .OrderBy(x => x.Value)
+                .Select(x => x.Key)
+                .First();
+
+            processed.Add(minimalCost);
+
+            foreach (var p in reachableFunction(minimalCost))
+            {
+                if (processed.Contains(p))
+                    continue;
+
+                var cost = minCosts[minimalCost] + costFunction(minimalCost, p);
+
+                if (minCosts.GetValueOrDefault(p, long.MaxValue) > cost)
+                {
+                    minCosts[p] = cost;
+                    bestPath[p] = minimalCost;
+                }
+
+                if (!endReached(p))
+                    continue;
+
+                totalCost = minCosts[p];
+
+                return true;
+            }
+
+            minCosts.Remove(minimalCost);
+        }
+
+        totalCost = long.MaxValue;
+        return false;
+    }
+
+
+
 }
---
+++ AdventOfCodeBase.Tests/PathFindingTests.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode.StandardAlgorithms;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCodeBase.Tests;
+
+public class PathFindingTests
+{
+    [Fact]
+    public void PathFinding_GetCostTest()
+    {
+        var pathFinding = new PathFinding<int>(i => new[] { i - 1, i + 1 });
+        pathFinding.GetCost(1, 100).Should().Be(99);
+    }
+
+    [Fact]
+    public void PathFinding_GetPath()
+    {
+        var pathFinding = new PathFinding<int>(i => new[] { i - 1, i + 1 });
+        var path = pathFinding.GetPath(1, 5);
+        path.Should().BeEquivalentTo(new[] { 1, 2, 3, 4, 5 }, c => c.WithStrictOrdering());
+    }
+
+
+
+}
