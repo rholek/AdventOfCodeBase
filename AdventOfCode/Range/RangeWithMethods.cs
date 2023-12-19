2ebe5f6f244cdda2d48059af030acabbc995708c
Radim Holek <rholek@idc.com>
2023-12-19 16:32:46 +0100

    Day 19

--- AdventOfCode/Range/RangeWithMethods.cs
+++ AdventOfCode/Range/RangeWithMethods.cs
@@ -1,7 +1,17 @@
 ﻿namespace AdventOfCode.Range;
+
 public record RangeWithMethods(long Start, long End)
 {
-    public bool Contains(long n) => n >= Start && n < End;
+    public static readonly RangeWithMethods Empty = new(0, -1);
+
+    public bool Contains(long n) => n >= Start && n <= End;
 
     public RangeWithMethods Offset(long offset) => new(Start - offset, End - offset);
+
+    public static implicit operator (long start, long end)(RangeWithMethods d) => (d.Start, d.End);
+    public static implicit operator RangeWithMethods((long start, long end) d) => new(d.start, d.end);
+
+    public bool IsEmpty => End < Start;
+
+    public long Size => End - Start + 1;
 }
