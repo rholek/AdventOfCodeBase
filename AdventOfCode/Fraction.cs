a2249c70a92f16bdb387061435e6bdc1d6f0521d
Radim Holek <rholek@idc.com>
2023-12-30 14:01:08 +0100

    Add Fraction

---
+++ AdventOfCode/Fraction.cs
@@ -0,0 +1,107 @@
+﻿using System.Numerics;
+
+namespace AdventOfCode;
+
+public readonly struct Fraction
+{
+    private readonly BigInteger numerator;
+    private readonly BigInteger denominator;
+    
+    public Fraction(BigInteger numerator, BigInteger denominator)
+    {
+        if (denominator < 0)
+        {
+            denominator *= -1;
+            numerator *= -1;
+        }
+
+        var divisor = BigInteger.GreatestCommonDivisor(denominator, numerator);
+        if (divisor < 0)
+            throw new InvalidOperationException();
+
+        this.numerator = numerator / divisor;
+        this.denominator = denominator / divisor;
+
+        if (this.denominator < 1)
+            throw new InvalidOperationException();
+    } 
+
+    public static Fraction Parse(string s)
+    {
+        var split = s.Split('/', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        if (split.Length == 1)
+            return new(split[0].AsLong(), 1);
+
+        if (split.Length != 2)
+            throw new ArgumentException($"Cannot parse '{split}'");
+
+        return new(split[0].AsLong(), split[1].AsLong());
+    }
+    
+    public static implicit operator Fraction(long d) => new(d, 1);
+    public static implicit operator Fraction(int d) => new(d, 1);
+    public static implicit operator long(Fraction d) => (long)(d.numerator / d.denominator);
+    public static implicit operator int(Fraction d) => (int)(d.numerator / d.denominator);
+    public static implicit operator double(Fraction d) => (double)d.numerator / (double)d.denominator; 
+    
+    public static Fraction operator +(Fraction first, Fraction second)
+    {
+        return new(first.numerator * second.denominator + second.numerator * first.denominator, first.denominator * second.denominator);
+    }
+
+    public static Fraction operator -(Fraction first, Fraction second)
+    {
+        return new(first.numerator * second.denominator - second.numerator * first.denominator, first.denominator * second.denominator);
+    }
+
+
+    public static Fraction operator -(Fraction first)
+    {
+        return new(-first.numerator, first.denominator);
+    }
+
+    public static Fraction operator *(Fraction first, Fraction second)
+    {
+        return new Fraction(first.numerator * second.numerator, first.denominator * second.denominator);
+    }
+
+    public static Fraction operator /(Fraction first, Fraction second)
+    {
+        return first * second.Invert();
+    }
+
+    public Fraction Invert() => new(denominator, numerator);
+    
+    public static bool operator ==(Fraction first, Fraction second)
+    {
+        return first.Equals(second);
+    }
+
+    public static bool operator !=(Fraction first, Fraction second)
+    {
+        return !first.Equals(second);
+    }
+
+    public bool Equals(Fraction other)
+    {
+        return numerator * denominator == other.numerator * other.denominator;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Fraction other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(numerator, denominator);
+    }
+
+    public override string ToString()
+    {
+        if (denominator == 1)
+            return numerator.ToString();
+
+        return $"{numerator}/{denominator}";
+    } 
+}
---
+++ AdventOfCodeBase.Tests/FractionTests.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCodeBase.Tests;
+
+public class FractionTests
+{
+    [Theory]
+    [InlineData("1/2", "1/2", "1")]
+    [InlineData("10", "15", "25")]
+    [InlineData("10", "-15", "-5")]
+    public void Fraction_AddTest(string first, string second, string result)
+    {
+        (Fraction.Parse(first) + Fraction.Parse(second)).Should().Be(Fraction.Parse(result));
+    }
+
+    [Theory]
+    [InlineData("1/2", "1/2", "0")]
+    [InlineData("10", "15", "-5")]
+    [InlineData("10", "-15", "25")]
+    public void Fraction_SubtractTest(string first, string second, string result)
+    {
+        (Fraction.Parse(first) - Fraction.Parse(second)).Should().Be(Fraction.Parse(result));
+    }
+
+    [Theory]
+    [InlineData("1/2", "1/2", "1/4")]
+    [InlineData("10", "15", "150")]
+    [InlineData("2/3", "-1", "-2/3")]
+    public void Fraction_MultiplyTest(string first, string second, string result)
+    {
+        (Fraction.Parse(first) * Fraction.Parse(second)).Should().Be(Fraction.Parse(result));
+    }
+
+    [Theory]
+    [InlineData("1/2", "1/2", "1")]
+    [InlineData("10", "15", "2/3")]
+    [InlineData("2/3", "-1", "-2/3")]
+    [InlineData("-1/2", "-1/2", "1")]
+    public void Fraction_DivideTest(string first, string second, string result)
+    {
+        (Fraction.Parse(first) / Fraction.Parse(second)).Should().Be(Fraction.Parse(result));
+    }
+}
