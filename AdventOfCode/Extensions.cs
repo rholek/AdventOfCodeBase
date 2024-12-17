b2a2cba6a48a08069c7685c315d05464920af685
Radim Holek <rholek@idc.com>
2024-12-17 12:47:26 +0100

    Add SplitByComma method

--- AdventOfCode/Extensions.cs
+++ AdventOfCode/Extensions.cs
@@ -262,6 +262,11 @@ public static class Extensions
         return input.Split(" ", removeEmpty ? StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries : StringSplitOptions.None);
     }
 
+    public static IEnumerable<string> SplitByComma(this string input, bool removeEmpty = true)
+    {
+        return input.Split(",", removeEmpty ? StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries : StringSplitOptions.None);
+    }
+
     public static IEnumerable<string> SplitByLine(this string input) => input.Split(Environment.NewLine);
 
     public static IEnumerable<string> SplitByDoubleLine(this string input) => input.Split($"{Environment.NewLine}{Environment.NewLine}");
